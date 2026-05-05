815cde5727af33b1dbc87b2fc31f5ce98a4c7f6d
Iuliia Morozova <yulii.morozova@gmail.com>
2026-05-05 13:08:40 +0200

    Initial Commit

---
+++ UIKitInSwiftUI/UIKitInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitInSwiftUIApp.swift
+//  UIKitInSwiftUI
+//
+//  Created by Julia Morozova on 5. 5. 2026..
+//
+
+import SwiftUI
+
+@main
+struct UIKitInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
